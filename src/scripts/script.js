2c31c761ebbe9e3f022d5cf5abca0dc4cb0533d1
alexander <alexanderhardeland@gmail.com>
2023-10-23 15:11:58 +0200

    Comment out errors

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,10 @@
-import getApiValues from './components/getApiValues.js';
-import apiLoader from './utilities/apiLoader.js';
+//import getApiValues from './components/getApiValues.js';
+//import apiLoader from './utilities/apiLoader.js';
+//import { mainSectionApiContainer } from './partials/variables.js';
 
-import { mainSectionApiContainer } from './partials/variables.js';
-
+/*
 apiLoader(mainSectionApiContainer);
 setTimeout( async function() {
   mainSectionApiContainer.append = await getApiValues();  
 }, 3000);
-
+*/
